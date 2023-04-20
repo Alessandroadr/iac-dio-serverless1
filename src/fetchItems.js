5c3390a03d56a94def51d20adeb2d85a5d25c074
Alessandro <arodriguesdoria@gmail.com>
2023-04-20 17:23:49 -0300

    first commit

---
+++ src/fetchItems.js
@@ -0,0 +1,28 @@
+"user strict"
+
+const AWS = require("aws-sdk");
+
+const fetchItems = async (event) => {
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+    let fetchItems;
+
+    try{
+        const results = await DynamoDB.scan({
+            TableName: "ItemTableNew"
+        }).promise();
+
+        items = results.Items;
+        
+    } catch(error){
+       console.log(error)
+    }
+    return{
+        statusCode: 200,
+        body: JSON.stringify(items),
+    };
+}
+
+module.exports = {
+    handler: fetchItems,
+};
